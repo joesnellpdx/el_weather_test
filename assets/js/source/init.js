23882088f8f710e43f2e9913eab1ebe87e2a00e3
Joe Snell <joesnellpdx@gmail.com>
2016-05-05 19:05:41 -0700

    main-commit

---
+++ assets/js/source/init.js
@@ -0,0 +1,185 @@
+(function($) {
+
+    var geoLocate = function(){
+        var geocoder;
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+        }
+//Get the latitude and the longitude;
+        function successFunction(position) {
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            codeLatLng(lat, lng)
+        }
+
+        function errorFunction(){
+            alert("Geocoder failed");
+        }
+
+        function initialize() {
+            geocoder = new google.maps.Geocoder();
+
+
+
+        }
+
+        function codeLatLng(lat, lng) {
+
+            var latlng = new google.maps.LatLng(lat, lng);
+            geocoder.geocode({'latLng': latlng}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    console.log(results)
+                    if (results[1]) {
+                        //formatted address
+                        alert(results[0].formatted_address)
+                        //find country name
+                        for (var i=0; i<results[0].address_components.length; i++) {
+                            for (var b=0;b<results[0].address_components[i].types.length;b++) {
+
+                                //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                                if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                                    //this is the object you are looking for
+                                    city= results[0].address_components[i];
+                                    break;
+                                }
+                            }
+                        }
+                        //city data
+                        alert(city.short_name + " " + city.long_name)
+
+
+                    } else {
+                        alert("No results found");
+                    }
+                } else {
+                    alert("Geocoder failed due to: " + status);
+                }
+            });
+        }
+    };
+
+    function geoFindMe() {
+        var output = document.getElementById("out");
+
+        if (!navigator.geolocation){
+            output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+            return;
+        }
+
+        function success(position) {
+            var latitude  = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+            var img = new Image();
+            img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+            output.appendChild(img);
+        };
+
+        function error() {
+            output.innerHTML = "Unable to retrieve your location";
+        };
+
+        output.innerHTML = "<p>Locating…</p>";
+
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+
+
+    var weatherFunction = function(){
+        var key = '8c1a8ef70cc596b2';
+        var Weather = 'http://api.wunderground.com/api/' + key + '/conditions/q/CA/San_Francisco.json';
+
+        var Geo={};
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success,error);
+        }
+        else {
+            alert('Geolocation is not supported');
+        }
+
+        function error() {
+            alert("That's weird! We couldn't find you!");
+        }
+        function success(position) {
+            Geo.lat = position.coords.latitude;
+            Geo.lng = position.coords.longitude;
+        }
+        $.ajax({
+            url : Weather,
+            dataType : "jsonp",
+            success : function(data) {
+                alert(data);
+                // get all the information
+            }
+        });
+
+        var location =data['location']['city'];
+        var temp = data['current_observation']['temp_f'];
+        var img = data['current_observation']['icon_url'];
+        var desc = data['current_observation']['weather'];
+        var wind = data['current_observation']['wind_string'];
+//setting the spans to the correct parameters
+        $('#location').html(location);
+        $('#temp').html(temp);
+        $('#desc').html(desc);
+        $('#wind').html(wind);
+//filling the image src attribute with the image url
+        $('#img').attr('src', img);
+    };
+
+    function codeLatLng(lat, lng) {
+
+        var latlng = new google.maps.LatLng(lat, lng);
+        geocoder.geocode({'latLng': latlng}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                console.log(results)
+                if (results[1]) {
+                    //formatted address
+                    alert(results[0].formatted_address)
+                    //find country name
+                    for (var i = 0; i < results[0].address_components.length; i++) {
+                        for (var b = 0; b < results[0].address_components[i].types.length; b++) {
+
+                            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+                            if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                                //this is the object you are looking for
+                                city = results[0].address_components[i];
+                                break;
+                            }
+                        }
+                    }
+                    //city data
+                    alert(city.short_name + " " + city.long_name)
+
+
+                } else {
+                    alert("No results found");
+                }
+            } else {
+                alert("Geocoder failed due to: " + status);
+            }
+        });
+    }
+
+    $(document).ready(function( $ ) {
+        geoFindMe();
+        codeLatLng();
+        alert('test');
+    });
+
+    $(window).load(function( $ ) {
+
+    });
+
+    $(window).resize(function() {
+    });
+
+    $(window).scroll(function() {
+
+    });
+
+})(jQuery);
---
+++ assets/js/source/plugins.js
@@ -0,0 +1,160 @@
+/*! jspdx_theme 1.0.0 plugins.js 2016-04-21 11:56:06 PM */
+(function($) {
+    "use strict";
+    $.fn.fitVids = function(options) {
+        var settings = {
+            customSelector: null,
+            ignore: null
+        };
+        if (!document.getElementById("fit-vids-style")) {
+            var head = document.head || document.getElementsByTagName("head")[0];
+            var css = ".fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}";
+            var div = document.createElement("div");
+            div.innerHTML = '<p>x</p><style id="fit-vids-style">' + css + "</style>";
+            head.appendChild(div.childNodes[1]);
+        }
+        if (options) {
+            $.extend(settings, options);
+        }
+        return this.each(function() {
+            var selectors = [ "iframe[src*='player.vimeo.com']", "iframe[src*='youtube.com']", "iframe[src*='youtube-nocookie.com']", "iframe[src*='kickstarter.com'][src*='video.html']", "object", "embed" ];
+            if (settings.customSelector) {
+                selectors.push(settings.customSelector);
+            }
+            var ignoreList = ".fitvidsignore";
+            if (settings.ignore) {
+                ignoreList = ignoreList + ", " + settings.ignore;
+            }
+            var $allVideos = $(this).find(selectors.join(","));
+            $allVideos = $allVideos.not("object object");
+            $allVideos = $allVideos.not(ignoreList);
+            $allVideos.each(function() {
+                var $this = $(this);
+                if ($this.parents(ignoreList).length > 0) {
+                    return;
+                }
+                if (this.tagName.toLowerCase() === "embed" && $this.parent("object").length || $this.parent(".fluid-width-video-wrapper").length) {
+                    return;
+                }
+                if (!$this.css("height") && !$this.css("width") && (isNaN($this.attr("height")) || isNaN($this.attr("width")))) {
+                    $this.attr("height", 9);
+                    $this.attr("width", 16);
+                }
+                var height = this.tagName.toLowerCase() === "object" || $this.attr("height") && !isNaN(parseInt($this.attr("height"), 10)) ? parseInt($this.attr("height"), 10) : $this.height(), width = !isNaN(parseInt($this.attr("width"), 10)) ? parseInt($this.attr("width"), 10) : $this.width(), aspectRatio = height / width;
+                if (!$this.attr("id")) {
+                    var videoID = "fitvid" + Math.floor(Math.random() * 999999);
+                    $this.attr("id", videoID);
+                }
+                $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent(".fluid-width-video-wrapper").css("padding-top", aspectRatio * 100 + "%");
+                $this.removeAttr("height").removeAttr("width");
+            });
+        });
+    };
+})(window.jQuery || window.Zepto);
+
+(function(root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(factory);
+    } else if (typeof exports === "object") {
+        module.exports = factory();
+    } else {
+        root.transformicons = factory();
+    }
+})(this || window, function() {
+    "use strict";
+    var tcon = {}, _transformClass = "tcon-transform", DEFAULT_EVENTS = {
+        transform: [ "click" ],
+        revert: [ "click" ]
+    };
+    var getElementList = function(elements) {
+        if (typeof elements === "string") {
+            return Array.prototype.slice.call(document.querySelectorAll(elements));
+        } else if (typeof elements === "undefined" || elements instanceof Array) {
+            return elements;
+        } else {
+            return [ elements ];
+        }
+    };
+    var getEventList = function(events) {
+        if (typeof events === "string") {
+            return events.toLowerCase().split(" ");
+        } else {
+            return events;
+        }
+    };
+    var setListeners = function(elements, events, remove) {
+        var method = (remove ? "remove" : "add") + "EventListener", elementList = getElementList(elements), currentElement = elementList.length, eventLists = {};
+        for (var prop in DEFAULT_EVENTS) {
+            eventLists[prop] = events && events[prop] ? getEventList(events[prop]) : DEFAULT_EVENTS[prop];
+        }
+        while (currentElement--) {
+            for (var occasion in eventLists) {
+                var currentEvent = eventLists[occasion].length;
+                while (currentEvent--) {
+                    elementList[currentElement][method](eventLists[occasion][currentEvent], handleEvent);
+                }
+            }
+        }
+    };
+    var handleEvent = function(event) {
+        tcon.toggle(event.currentTarget);
+    };
+    tcon.add = function(elements, events) {
+        setListeners(elements, events);
+        return tcon;
+    };
+    tcon.remove = function(elements, events) {
+        setListeners(elements, events, true);
+        return tcon;
+    };
+    tcon.transform = function(elements) {
+        getElementList(elements).forEach(function(element) {
+            element.classList.add(_transformClass);
+        });
+        return tcon;
+    };
+    tcon.revert = function(elements) {
+        getElementList(elements).forEach(function(element) {
+            element.classList.remove(_transformClass);
+        });
+        return tcon;
+    };
+    tcon.toggle = function(elements) {
+        getElementList(elements).forEach(function(element) {
+            tcon[element.classList.contains(_transformClass) ? "revert" : "transform"](element);
+        });
+        return tcon;
+    };
+    return tcon;
+});
+
+(function() {
+    var method;
+    var noop = function noop() {};
+    var methods = [ "assert", "clear", "count", "debug", "dir", "dirxml", "error", "exception", "group", "groupCollapsed", "groupEnd", "info", "log", "markTimeline", "profile", "profileEnd", "table", "time", "timeEnd", "timeStamp", "trace", "warn" ];
+    var length = methods.length;
+    var console = window.console = window.console || {};
+    while (length--) {
+        method = methods[length];
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+})();
+
+(function($) {
+    var fitVideo = function() {
+        $(".video, .tm-vid, .jspdx-video").fitVids();
+        $(".entry-content").fitVids();
+    };
+    var navToggleAnimate = function() {
+        transformicons.add(".tcon");
+    };
+    $(document).ready(function($) {
+        fitVideo();
+        navToggleAnimate();
+    });
+    $(window).load(function($) {});
+    $(window).resize(function() {});
+    $(window).scroll(function() {});
+})(jQuery);
